10dd5278aa8aacd8bdbc53f2470cf950b8d124ed
Marcel Lüttecke <mluettecke@posteo.net>
2023-12-01 17:41:50 +0100

    🗑️ Remove aoc 2015 test code.

--- src/advent_of_code/mod.rs
+++ src/advent_of_code/mod.rs
@@ -1,14 +1,11 @@
 use self::utils::read_input;
 
-mod aoc2015;
 mod utils;
 
 pub fn run_solution(year: u16, day: u8, part: u8) -> Result<String, std::io::Error> {
     let input = &read_input(year, day);
 
-    let result = match (year, day, part) {
-        (2015, 1, 1) => aoc2015::day01::part_one(&aoc2015::day01::generator(input)),
-        (2015, 1, 2) => aoc2015::day01::part_two(&aoc2015::day01::generator(input)),
+    let result: i32 = match (year, day, part) {
         _ => {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
