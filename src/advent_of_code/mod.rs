ed09412a280b77b4ac08655c4b724e88a10cca8c
Kurandur <4482510+Kurandur@users.noreply.github.com>
2024-12-16 21:29:23 +0100

    ✨feat: Change way the aoc results are handled so that u32 and u64 work

--- src/advent_of_code/mod.rs
+++ src/advent_of_code/mod.rs
@@ -7,27 +7,67 @@ mod utils;
 pub fn run_solution(year: u16, day: u8, part: u8) -> Result<String, std::io::Error> {
     let input = &read_input(year, day);
 
-    let result: u32 = match (year, day, part) {
-        (2023, 1, 1) => aoc2023::day01::part_one(&aoc2023::day01::generator(input)),
-        (2023, 1, 2) => aoc2023::day01::part_two(&aoc2023::day01::generator(input)),
-        (2023, 2, 1) => aoc2023::day02::part_one(aoc2023::day02::generator(input)),
-        (2023, 2, 2) => aoc2023::day02::part_two(aoc2023::day02::generator(input)),
-        (2023, 3, 1) => aoc2023::day03::part_one(&aoc2023::day03::generator(input)),
-        (2023, 3, 2) => aoc2023::day03::part_two(&aoc2023::day03::generator(input)),
-        (2023, 4, 1) => aoc2023::day04::part_one(aoc2023::day04::generator(input)),
-        (2023, 4, 2) => aoc2023::day04::part_two(&aoc2023::day04::generator(input)),
-        (2024, 1, 1) => aoc2024::day01::part_one(&aoc2024::day01::generator(input)),
-        (2024, 1, 2) => aoc2024::day01::part_two(&aoc2024::day01::generator(input)),
-        (2024, 2, 1) => aoc2024::day02::part_one(&aoc2024::day02::generator(input)),
-        (2024, 2, 2) => aoc2024::day02::part_two(&aoc2024::day02::generator(input)),
-        (2024, 3, 1) => aoc2024::day03::part_one(&aoc2024::day03::generator(input)),
-        (2024, 3, 2) => aoc2024::day03::part_two(&aoc2024::day03::generator(input)),
-        (2024, 4, 1) => aoc2024::day04::part_one(&aoc2024::day04::generator(input)),
-        (2024, 4, 2) => aoc2024::day04::part_two(&aoc2024::day04::generator(input)),
-        (2024, 5, 1) => aoc2024::day05::part_one(&aoc2024::day05::generator(input)),
-        (2024, 5, 2) => aoc2024::day05::part_two(&aoc2024::day05::generator(input)),
-        (2024, 6, 1) => aoc2024::day06::part_one(&aoc2024::day06::generator(input)),
-        (2024, 6, 2) => aoc2024::day06::part_two(&aoc2024::day06::generator(input)),
+    let result = match (year, day, part) {
+        (2023, 1, 1) => {
+            SolutionResult::U32(aoc2023::day01::part_one(&aoc2023::day01::generator(input)))
+        }
+        (2023, 1, 2) => {
+            SolutionResult::U32(aoc2023::day01::part_two(&aoc2023::day01::generator(input)))
+        }
+        (2023, 2, 1) => {
+            SolutionResult::U32(aoc2023::day02::part_one(aoc2023::day02::generator(input)))
+        }
+        (2023, 2, 2) => {
+            SolutionResult::U32(aoc2023::day02::part_two(aoc2023::day02::generator(input)))
+        }
+        (2023, 3, 1) => {
+            SolutionResult::U32(aoc2023::day03::part_one(&aoc2023::day03::generator(input)))
+        }
+        (2023, 3, 2) => {
+            SolutionResult::U32(aoc2023::day03::part_two(&aoc2023::day03::generator(input)))
+        }
+        (2023, 4, 1) => {
+            SolutionResult::U32(aoc2023::day04::part_one(aoc2023::day04::generator(input)))
+        }
+        (2023, 4, 2) => {
+            SolutionResult::U32(aoc2023::day04::part_two(&aoc2023::day04::generator(input)))
+        }
+        (2024, 1, 1) => {
+            SolutionResult::U32(aoc2024::day01::part_one(&aoc2024::day01::generator(input)))
+        }
+        (2024, 1, 2) => {
+            SolutionResult::U32(aoc2024::day01::part_two(&aoc2024::day01::generator(input)))
+        }
+        (2024, 2, 1) => {
+            SolutionResult::U32(aoc2024::day02::part_one(&aoc2024::day02::generator(input)))
+        }
+        (2024, 2, 2) => {
+            SolutionResult::U32(aoc2024::day02::part_two(&aoc2024::day02::generator(input)))
+        }
+        (2024, 3, 1) => {
+            SolutionResult::U32(aoc2024::day03::part_one(&aoc2024::day03::generator(input)))
+        }
+        (2024, 3, 2) => {
+            SolutionResult::U32(aoc2024::day03::part_two(&aoc2024::day03::generator(input)))
+        }
+        (2024, 4, 1) => {
+            SolutionResult::U32(aoc2024::day04::part_one(&aoc2024::day04::generator(input)))
+        }
+        (2024, 4, 2) => {
+            SolutionResult::U32(aoc2024::day04::part_two(&aoc2024::day04::generator(input)))
+        }
+        (2024, 5, 1) => {
+            SolutionResult::U32(aoc2024::day05::part_one(&aoc2024::day05::generator(input)))
+        }
+        (2024, 5, 2) => {
+            SolutionResult::U32(aoc2024::day05::part_two(&aoc2024::day05::generator(input)))
+        }
+        (2024, 6, 1) => {
+            SolutionResult::U32(aoc2024::day06::part_one(&aoc2024::day06::generator(input)))
+        }
+        (2024, 6, 2) => {
+            SolutionResult::U32(aoc2024::day06::part_two(&aoc2024::day06::generator(input)))
+        }
         _ => {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
@@ -35,5 +75,10 @@ pub fn run_solution(year: u16, day: u8, part: u8) -> Result<String, std::io::Err
             ))
         }
     };
-    Ok(result.to_string())
+    let result_string = match result {
+        SolutionResult::U32(value) => value.to_string(),
+        SolutionResult::U64(value) => value.to_string(),
+    };
+
+    Ok(result_string)
 }
